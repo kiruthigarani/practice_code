0a73449ab9903bb4bfa6cc05bbc946ab803e8c54
kiruthiga <kiruthigarani.kannan@capgemini.com>
2026-01-14 12:28:11 +0530

    indexOf - coding practice

--- findCommonNumbers.js
+++ findCommonNumbers.js
@@ -3,6 +3,7 @@ const a = [1,2,3,4,5,6,3];
 const b = [3,4,3,8,9,0];
 
 const commonvalues =[... new Set(a.filter((element)=>{
+    console.log("Element includes:",b.includes(element));
  return b.includes(element);
 }))];
 
--- removeDuplicateNumbers.js
+++ removeDuplicateNumbers.js
@@ -1,6 +1,8 @@
-const list = [1,2,3,4,1,2,3,1,2,3,4,5];
+const list = [1,2,3,4,3,2,1,1,2,3,4,5];
 
+//indexOf method returns the index of the first occurrence of the element in the array
 const newList = list.filter((element,index)=>{
+    console.log("Element:",element,"--","Index of element:",list.indexOf(element),"--","Current Index:",index)  ;
   return list.indexOf(element) === index
 });
 
@@ -9,3 +11,26 @@ console.log(newList);
 //alternative solution
 const uniqueList = [...new Set(list)];
 console.log("uniqueList:", uniqueList);
+
+const str1 = "Hello world, welcome to the world.";
+console.log(str1.indexOf("world")); // returns 6
+console.log(str1.indexOf("World")); // returns -1, CASE SENSITIVE
+
+//how to check the count of the substring exists in a string
+
+const string = "Hello world, welcome to the world.";
+const subStr = "world";
+const count = (string, subStr) => {
+    let count = 0;
+    let pos = string.indexOf(subStr);
+    while (pos !== -1) {
+        count++;
+        pos = string.indexOf(subStr, pos + 1);
+    }
+    return count;
+};
+const count_of_substring = count(string, subStr);
+console.log(`The substring "${subStr}" occurs ${count_of_substring} times in the string.`);
+
+const str = "Hello world, welcome to the world.";
+console.log(str.indexOf("world", 10)); // RETURNS 28, STARTING SEARCH FROM INDEX 10
