7888bb6209d574d099cc40f5333c624fc21a8f69
kiruthiga <kiruthigarani.kannan@capgemini.com>
2026-01-20 13:31:08 +0530

    remove duplicate from result array

--- findCommonNumbers.js
+++ findCommonNumbers.js
@@ -2,9 +2,10 @@
 const a = [1,2,3,4,5,6,3];
 const b = [3,4,3,8,9,0];
 
-const commonvalues =[... new Set(a.filter((element)=>{
+const commonvalues = [new Set(a.filter((element)=>{
     console.log("Element includes:",b.includes(element));
- return b.includes(element);
+    const isCommon = b.includes(element);
+ return isCommon;
 }))];
 
 console.log(commonvalues);
