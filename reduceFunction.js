d2e6284ed13fb74ae48f384add84dcd4e07510ce
kiruthigaranik <kiruthigaranik@gmail.com>
2026-01-30 18:18:54 +0530

    map,reduce,filter

---
+++ reduceFunction.js
@@ -0,0 +1,59 @@
+const vals = [1, 2, 3, 4, 5];
+let sum =0;
+
+for(let i=0;i<=vals.length-1;i++){
+      sum = sum + vals[i];
+}
+console.log("sum using for loop:",sum);
+
+let reduecedsum = vals.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+}, 0);
+
+console.log("sum using reduce function:",reduecedsum);
+
+const person = [{
+    name: "Alice",
+    age: 30,
+    city: "New York"
+}, {
+    name: "Bob",
+    age: 25,
+    city: "Los Angeles"
+}, {
+    name: "Charlie",
+    age: 35,
+    city: "New York"
+},
+{    name: "David",
+    age: 28,
+    city: "Chicago"
+},
+{    name: "Eve",
+    age: 22,
+    city: "Los Angeles"
+}
+];
+//group by city
+const groupByCity = person.reduce((acc, curr) => {
+  
+    console.log('Accumulator:', acc[curr.city]);
+    if (acc[curr.city]) {
+        acc[curr.city] = ++acc[curr.city];
+    } else {
+        acc[curr.city] = 1;
+    }
+   
+   return acc;
+}, {});
+
+console.log(groupByCity);
+
+const getName = person.filter( (p) =>{
+    if(p.age >= 30){
+        return p.name;
+    }
+});
+console.log("Names of persons with age >= 30:",getName);
+const names = person.map( (p) => p.name );
+console.log("Names of all persons:",names);
