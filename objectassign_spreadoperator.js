a9a227d5a09dd9f4365fbe5c08f6c835dbc50ab8
kiruthiga <kiruthigarani.kannan@capgemini.com>
2026-04-17 11:43:18 +0530

    string literal & objectassign& spread operator

--- objectassign_spreadoperator.js
+++ objectassign_spreadoperator.js
@@ -7,4 +7,12 @@ console.log(merging_assign,obj1); //{a: 1, b: 3, c: 4}  {a: 1, b: 3, c: 4}
 let merging_spread = {...obj1,...obj2};
 console.log(merging_spread,obj1); // {a: 1, b: 3, c: 4} {a: 1, b: 2}
 
+//string literal
+const laptopModel = 'HP';
+const deskNo = '4';
+const name = 'kiruthiga';
+const problem = 'Facing VPN issue';
 
+const message = `Hello, I am ${name}. I am using ${laptopModel} laptop,
+and my desk number is ${deskNo}. I am facing the following problem: ${problem}.`;
+console.log(message);
